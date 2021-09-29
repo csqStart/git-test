9363cf14141c2c8b661e5830d9d116d35615621f
zerlinda <727158915@qq.com>
2021-09-29 09:55:05 +0800

    second version

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -1,5 +1,7 @@
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("version1111");
+        System.out.println("version1111");
+        System.out.println("version1111");
     }
 }
